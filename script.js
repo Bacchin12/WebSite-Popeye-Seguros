31e295a9b342c1ce4aac7dc8f8c579afadd7e866
Victor Bacchin <victor.bacchin@fatec.sp.gov.br>
2024-10-08 15:40:16 -0300

    primeira versão do site

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+document.getElementById('saibaMaisBtn1').addEventListener('click', function() {
+    window.location.href = '#servico1';
+
+    
+});
+
+document.getElementById('saibaMaisBtn2').addEventListener('click', function() {
+    window.location.href = '#servico2';
+
+    
+});
+
+document.getElementById('saibaMaisBtn3').addEventListener('click', function() {
+    window.location.href = '#servico3'; 
+
+    
+});
+
+document.getElementById('saibaMaisBtn4').addEventListener('click', function() {
+    window.location.href = '#servico4'; 
+
+    
+});
+
+document.getElementById('saibaMaisBtn5').addEventListener('click', function() {
+    window.location.href = '#servico5'; 
+
+    
+});
+
+document.getElementById('saibaMaisBtn6').addEventListener('click', function() {
+    window.location.href = '#servico6'; 
+
+    
+});
+
+}); 
